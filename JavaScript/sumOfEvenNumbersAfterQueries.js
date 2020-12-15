bdbb7f4ee279d47e120fc69772f8d93fbffad376
jarjanazy <celil.cercenazi@gmail.com>
2020-12-15 19:33:09 +0200

    Add the solution for get sum of even numbers after query

---
+++ JavaScript/sumOfEvenNumbersAfterQueries.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} A
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var sumEvenAfterQueries = function(A, queries) {
+    let result = []
+    queries.forEach(function(list, index, element){
+        performQuery(A, element);
+        result[index] = getSumOfEvenNumbers(A);
+    })
+    return result;
+};
+
+
+function performQuery(A, query){
+    A[query[1]] = query[0];
+}
+
+function getSumOfEvenNumbers(list){
+    return list.reduce(function(total, currentValue){
+        if (currentValue % 2 === 0) total += currentValue;
+        return total;
+    });
+}
