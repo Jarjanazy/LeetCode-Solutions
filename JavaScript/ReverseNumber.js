b80ae0bd95419f750264f2066f1364f12aa89dd1
jarjanazy <celil.cercenazi@gmail.com>
2020-12-19 15:05:41 +0300

    Make the NumberReverse handle the 32bit overflows

--- JavaScript/ReverseNumber.js
+++ JavaScript/ReverseNumber.js
@@ -6,5 +6,12 @@ var reverse = function(x) {
     if (x === 0) return 0;
     let absoulteNumber = (x < 0) ? -1 * x : x;
     let absoulteResult = Number(absoulteNumber.toString().split("").reverse().join(""));
+
+    if (overflows(absoulteResult)) return 0;
+
     return absoulteResult * x / absoulteNumber;
 };
+
+function overflows(num){
+    return Math.imul(num, 1) !== num;
+}
