7c7193d9a7978212ed45568398082144e9def126
jarjanazy <celil.cercenazi@gmail.com>
2020-12-20 17:35:33 +0300

    fix the solution for the longestSubstring problem

--- JavaScript/LongestSubstringWithoutReapetingChars.js
+++ JavaScript/LongestSubstringWithoutReapetingChars.js
@@ -3,18 +3,26 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const set = new Set();
     let maxCount = 0;
-    let updatedCount = 0;
+    let startIndex = 0;
+    let list = s.split("");
 
-    [...s].forEach(function(char){
-        if (set.has(char)){
-            maxCount = Math.max(maxCount, updatedCount);
-            updatedCount = 0;
-            set.clear();
-        }
-        set.add(char);
-        updatedCount++;
-    });
-    return Math.max(maxCount, updatedCount);
+    for (let i = 0; i < list.length; i++){
+        let countFromCurrentIndex = countSubStringWithoutReapaetingChars(i, list);
+        maxCount = Math.max(maxCount, countFromCurrentIndex);
+    }
+    return maxCount;
 };
+
+function countSubStringWithoutReapaetingChars(startIndex, list){
+    let count = 0;
+    const set = new Set();
+    for (let i = startIndex; i < list.length; i++){
+        if (set.has(list[i])) return count;
+        else{
+            count++;
+            set.add(list[i]);
+        }
+    }
+    return count;
+}
