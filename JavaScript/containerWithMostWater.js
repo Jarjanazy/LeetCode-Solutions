6b560a2d0640ae0d27885aa363871f1d02331aba
jarjanazy <celil.cercenazi@gmail.com>
2020-12-25 20:21:02 +0300

    Add the solution for the containerWithMostWater

--- JavaScript/containerWithMostWater.js
+++ JavaScript/containerWithMostWater.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let maxCapacity = 0;
+    let maxCapacityForIndex;
+    for (let i=0; i < height.length; i++){
+        maxCapacityForIndex = getMaxCapacityForIndex(height, i);
+        maxCapacity = Math.max(maxCapacity, maxCapacityForIndex);
+    }
+    return maxCapacity;
 };
+
+function getMaxCapacityForIndex(height, startIndex){
+    let maxResult = 0;
+    let currentResult;
+    let endPoint;
+    let heightOfWall;
+    for (let i = startIndex; i < height.length; i++){
+        endPoint = height[i];
+        heightOfWall = Math.min(endPoint, height[startIndex]);
+        maxResult = Math.max(maxResult, heightOfWall * (i - startIndex));
+    }
+    return maxResult;
+}
