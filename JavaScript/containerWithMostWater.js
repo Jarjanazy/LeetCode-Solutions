f91ec5988a59364fba575b9d5847d3e8d581a3f9
jarjanazy <celil.cercenazi@gmail.com>
2020-12-25 20:23:22 +0300

    Remove unccessary code from the containerWithMostWater

--- JavaScript/containerWithMostWater.js
+++ JavaScript/containerWithMostWater.js
@@ -4,19 +4,15 @@
  */
 var maxArea = function(height) {
     let maxCapacity = 0;
-    let maxCapacityForIndex;
     for (let i=0; i < height.length; i++){
-        maxCapacityForIndex = getMaxCapacityForIndex(height, i);
-        maxCapacity = Math.max(maxCapacity, maxCapacityForIndex);
+        maxCapacity = Math.max(maxCapacity, getMaxCapacityForIndex(height, i));
     }
     return maxCapacity;
 };
 
 function getMaxCapacityForIndex(height, startIndex){
     let maxResult = 0;
-    let currentResult;
-    let endPoint;
-    let heightOfWall;
+    let endPoint,heightOfWall;
     for (let i = startIndex; i < height.length; i++){
         endPoint = height[i];
         heightOfWall = Math.min(endPoint, height[startIndex]);
