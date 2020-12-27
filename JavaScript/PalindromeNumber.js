e645ba1b6a8a958c540eafbc1b041d85bfdb48c6
jarjanazy <celil.cercenazi@gmail.com>
2020-12-27 22:55:43 +0300

    initial attempt to solve the Palindrome problem

--- JavaScript/PalindromeNumber.js
+++ JavaScript/PalindromeNumber.js
@@ -3,5 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    let num = x.toString().split("");
+    if (num.length < 2) return true;
+    let currentNum;
+    let matchingNum;
+    const windowSize = Math.floor(num.length / 2);
+    const extraWindow = num.length % 2 == 0 ? 0 : 1;
+
+    for (let i = 0; i <= (num.length / 2 )- 1; i++){
+        currentNum = num[i];
+        matchingNum = num[i + windowSize + extraWindow];
+        console.log(i, i+windowSize);
+        if (currentNum != matchingNum) return false;
+    }
+    return true;
 };
