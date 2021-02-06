04dab434f9381d18ab1893e8249911e0784d0ff9
jarjanazy <celil.cercenazi@gmail.com>
2021-02-06 10:50:58 +0300

    add the solution file for the maximum product subarray problem, the solution doesnt work for large inputs

--- Python/Maximum Product Subarray.py
+++ Python/Maximum Product Subarray.py
@@ -1,6 +1,21 @@
+from functools import reduce
+
 class Solution(object):
     def maxProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        if (len(nums) == 1):
+            return nums[0]
+
+        currentMaxProduct = float('-inf')
+        for currentIndex in range (0, len(nums)):
+            if (nums[currentIndex] == 0):
+                    continue
+            for window in range (currentIndex + 1, len(nums)+1):
+                localMaxProduct = reduce(lambda x, y : x * y, nums[currentIndex : window])
+                print(localMaxProduct)
+                currentMaxProduct = max(currentMaxProduct, localMaxProduct)
+        
+        return currentMaxProduct
